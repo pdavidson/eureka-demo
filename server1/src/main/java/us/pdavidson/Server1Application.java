a663cfd6da340223dbe6d5101a9251bef96c34e0
Peter Davidson <pdavidso@gmail.com>
2015-07-25 16:15:57 -0600

    Fixed Mapping

--- server1/src/main/java/us/pdavidson/Server1Application.java
+++ server1/src/main/java/us/pdavidson/Server1Application.java
@@ -3,12 +3,20 @@ package us.pdavidson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class Server1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Server1Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "hello world\n" ;
     }
 }
